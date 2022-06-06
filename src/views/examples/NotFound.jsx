ac2386eb8cef25ea06112bc2cc56f81e8b21095d
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-06 13:05:19 -0300

    PageNotFound

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default ()=>{
+
+    return(
+
+        <div className="NotFound">
+            <h1>404</h1>
+            <h3>Opss... Pagina não encontrada!</h3>
+        </div>
+    )
+}
